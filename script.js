d56de9a04614c70de3148f4d23cec080ad865448
S Sankavi <94035278+SankaviS@users.noreply.github.com>
2021-12-14 21:56:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let name = "swetha"
+// switch (name) {
+//     case "sankavi":
+//         console.log("good girl");
+//         break;
+//     case "swetha":
+//         console.log("bad girl");
+//         break;
+//     default:
+//         console.log("no name");
+// }
